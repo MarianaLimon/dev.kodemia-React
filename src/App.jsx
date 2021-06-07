2463a9741a77ee8236b8ee908c82233acafce2b3
MarianaLimon <marianalimong@hotmail.com>
2021-06-07 17:07:15 -0500

    ADD Challenge

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,9 @@ import React from "react";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+//Screens
 import Home from "./screens/Home";
+import Challenge from "./screens/Challenge";
 class App extends React.Component {
   render() {
     return (
@@ -11,6 +13,9 @@ class App extends React.Component {
           <Switch>
             <Route exact path="/">
               <Home />
+            </Route>
+            <Route exact oath="/challenge">
+              <Challenge />
             </Route>
           </Switch>
         </div>
